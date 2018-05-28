e72ccb65f4d51f7113570f741d48f8f2cd7016a8
Tolu Oluwagbemi <tulburg2@gmail.com>
2018-05-28 13:22:09 +0100

    starting settings

---
+++ src/components/Settings.vue
@@ -0,0 +1,13 @@
+<template>
+	<section class="settings">
+		<h1>Settings</h1>
+		<router-view></router-view>
+	</section>
+</template>
+
+
+<script>
+	export default {
+		name: 'Settings'
+	}
+</script>
