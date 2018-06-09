ae865bab94db05ed07157a3b075e17edb510e4e0
Tolu Oluwagbemi <tulburg2@gmail.com>
2018-06-09 14:16:09 +0100

    fixing heroku routing

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var path = require('path');
+var serveStatic = require('serve-static');
 app = express();
-app.use('/dist', express.static( __dirname+'/dist'));
+app.use(serveStatic(__dirname + "/dist"));
+
 app.get('/*', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/dist/index.html');
 });
+
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
